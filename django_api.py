9f8be47ab941bd5c9b5e9817b53e1e1b47475c1a
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2026-03-21 18:15:09 -0400

    new error handeling try/catch block created within new 'get_user_info' function.

--- django_api.py
+++ django_api.py
@@ -1,9 +1,13 @@
-import django_api
+from django.db import models
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from myapp.serializers import UserSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
 
 
 @api_view(["GET", "POST"])
@@ -22,3 +26,20 @@ def snippet_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view("GET", "EDIT")    
+class User(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_user_info(self, user_info, ):
+        
+        self.get_user_info = user_info
+        try:
+            user_info is not None
+            return(user_info)
+        except:
+            return("User not found. Please try the api call again.")
+    
+
