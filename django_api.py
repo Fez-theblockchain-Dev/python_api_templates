ef3d14d2717fec4b713184285b5d12640ddbba8e
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2026-03-22 11:20:00 -0400

    created a router class using the django api docs 'UserViewSet' template this is now housed within the 'Router' class. Also creating a set action function within the router class.

--- django_api.py
+++ django_api.py
@@ -41,5 +41,36 @@ class User(generics.ListCreateAPIView):
             return(user_info)
         except:
             return("User not found. Please try the api call again.")
+        
+
+class router(viewsets.ViewSet):
+
+    def list(self, request):
+        self.request()
+
+    @action(detail=True, methods=['post'])
+    def set_password(self, request, pk=None):
+        user = self.get_object()
+        serializer = PasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user.set_password(serializer.validated_data['password'])
+            user.save()
+            return Response({'status': 'password set'})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
     
 
