f6aa18acbf1fe9ba89a7c51ab27817cb835cf894
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2026-03-30 00:00:20 -0400

    Added more boilerplate functions in models.py for auth settings of apps  period.

--- django_api.py
+++ django_api.py
@@ -49,7 +49,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-   
+    
 # Routers provide an easy way of automatically determining the URL conf.
     router = routers.DefaultRouter()
     router.register(r"users", UserViewSet)
