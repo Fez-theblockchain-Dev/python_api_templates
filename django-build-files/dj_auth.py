68a20087ad6be030e1a4f03412eae8958647c244
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2026-03-31 17:26:22 -0400

    added new transformers file which will extend the 'UserViewSet' template.

--- django-build-files/dj_auth.py
+++ django-build-files/dj_auth.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
