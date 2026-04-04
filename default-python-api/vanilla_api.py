18b8bc506e09c242f4744b8228a067578db93aaa
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2026-04-03 22:07:38 -0400

    New get_data function which will check if there is a successful connection to the db which houses the data for future

--- default-python-api/vanilla_api.py
+++ default-python-api/vanilla_api.py
@@ -1,7 +1,8 @@
 # Standard library only
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
-from socketserver import ThreadingMixIn, , 
+import requests
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn, ThreadingTCPServer
 
 class ThreadedServer(ThreadingMixIn, HTTPServer):
     def __init__(self, server_address, RequestHandlerClass):
@@ -10,6 +11,16 @@ class ThreadedServer(ThreadingMixIn, HTTPServer):
         self.next_id = 1
         
         return ThreadedServer(server_address, RequestHandlerClass)
+    
+
+    def get_data(self, data, db)
+        data = pychon.loads(socketserver.BaseHTTPRequestHandler)
+        for key, data in data.items():
+            if key.pressed == "ENTER | RETURN : KEY_PRESSED = ENTER | RETURN":
+                return (200, db)
+            print("successful server response")
+
+            
 
 
 
